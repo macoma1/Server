25d6d765db1f94727579169dc45a8faf020773b2
mariocorral <mariotwitter2012@gmail.com>
2023-11-09 00:38:46 +0100

    LAST COMMIT

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const commentRoutes = require('./routes/comments.routes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
