6caca3a4024a2cc913fddaf3d49aecd7b75172c8
mariocorral <mariotwitter2012@gmail.com>
2023-11-09 01:18:23 +0100

    asd

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db.config');
-const userRoutes = require('./routes/user.routes');
-const cors = require('cors');
-const commentRoutes = require('./routes/comments.routes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.config");
+const userRoutes = require("./routes/user.routes");
+const cors = require("cors");
+const commentRoutes = require("./routes/comments.routes");
 
 dotenv.config();
 
@@ -13,26 +13,28 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-const allowedOrigins = process.env.allowedOrigins;
-app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'La política de CORS para este sitio no permite acceso desde el origen especificado.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
-
+const allowedOrigins = "https://videogames-3ce93.web.app";
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(
+          new Error(
+            "La política de CORS para este sitio no permite acceso desde el origen especificado."
+          )
+        );
+      }
+    },
+  })
+);
 
 connectDB();
 
 app.use(commentRoutes);
 app.use(userRoutes);
 
-
-
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
